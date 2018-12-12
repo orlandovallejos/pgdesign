4e82fefa1d920fd801a808ff72b429b7b31dfcf6
Orlando Vallejos <orlandovallejos@gmail.com>
2018-12-11 22:55:45 -0300

    First version of the red login page.

--- subsites/login/src/App.js
+++ subsites/login/src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Col, Row, Input } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+        <Row>
+          <Col span={16}>
+            <div>
+              <h1>We create a</h1>
+              <h1>New Generation</h1>
+              <h1>of Dream</h1>
+            </div>
+            <div>
+              <p>Lorem asd asd asd</p>
+            </div>
+          </Col>
+          <Col span={8}>
+            <div className="loginWrapper">
+              <h4>Welcome back</h4>
+              <label htmlFor="">Email</label>
+              <Input size="large" placeholder="email" />
+
+              <label htmlFor="">Password</label>
+              <Input size="large" placeholder="password" />
+            </div>
+          </Col>
+        </Row>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Facebook - Twitter
+        </Footer>
+      </Layout>
     );
   }
 }
